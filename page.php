f189be0f60beb1359d4e16eaac8f3f30bc36a388
Melin Matthieu <73657051+matthieumelin@users.noreply.github.com>
2021-05-11 18:59:17 +0200

    Remove un-used line in page.php

--- page.php
+++ page.php
@@ -5,8 +5,6 @@
     while (have_posts()) :
         the_post();
 
-        get_template_part('template-parts/content/content', 'page');
-
         if (comments_open() || get_comments_number()) {
             comments_template();
         }
